3f23dd091b66b7f55fa5485dcf0672f927505f54
rcm-webdev <rcm.webdev.pro@gmail.com>
2023-11-08 00:18:22 -0800

    updated astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://rcm-webdev.github.io",
+  base: "/wds.github.io",
   integrations: [tailwind()],
 });
