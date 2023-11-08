1bda04dfca44d74061fbcb70745bbaa9f4f49715
rcm-webdev <rcm.webdev.pro@gmail.com>
2023-11-08 00:21:57 -0800

    removed base from astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://wds.github.io",
-  base: "/wds.github.io",
+
   integrations: [tailwind()],
 });
